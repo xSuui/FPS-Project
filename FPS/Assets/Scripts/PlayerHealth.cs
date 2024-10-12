6199164b72cb41432efefe7d5aae176aa859c546
xSuui <samuel2012rogerio@gmail.com>
2024-10-12 00:11:18 -0300

    add life to player

--- FPS/Assets/Scripts/PlayerHealth.cs
+++ FPS/Assets/Scripts/PlayerHealth.cs
@@ -5,11 +5,33 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int maxHealth = 100;
     public int health = 100;
     public Image bloodImage;
     public Image redImage;
 
+    public int recoveryFactor = 20;
+
+    public float recoveryRate = 5f;
+    private float recoveryTimer;
+
     private Color alphaAmount;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        recoveryTimer += Time.deltaTime;
+
+        if(recoveryTimer > recoveryRate)
+        {
+            StartCoroutine(RecoveryHealth());
+        }
+    }
+
 
     public void ApplyDamage(int damage)
     {
@@ -30,6 +52,23 @@ public class PlayerHealth : MonoBehaviour
         {
             Debug.Log("game over!");
         }
+
+        recoveryTimer = 0f;
+    }
+
+    IEnumerator RecoveryHealth()
+    {
+        while(health < maxHealth)
+        {
+            health += recoveryFactor;
+
+            alphaAmount.a -= ((float)recoveryFactor / 100);
+
+            bloodImage.color = alphaAmount;
+            redImage.color = new Color(255f, 0f, 0f, alphaAmount.a);
+            yield return new WaitForSeconds(2f);
+        }
+
 
     }
 }
